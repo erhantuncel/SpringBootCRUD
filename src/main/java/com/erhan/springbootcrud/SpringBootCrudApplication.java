fe25cccad0329520c18d7adb8c3b01112ae84bf6
Erhan TUNCEL <yazilim.erhan@gmail.com>
2019-07-13 21:40:06 +0300

    Added: Beans to set locale resolver and message source.

--- src/main/java/com/erhan/springbootcrud/SpringBootCrudApplication.java
+++ src/main/java/com/erhan/springbootcrud/SpringBootCrudApplication.java
@@ -1,13 +1,53 @@
 package com.erhan.springbootcrud;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class SpringBootCrudApplication {
+@EnableAutoConfiguration
+public class SpringBootCrudApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.forLanguageTag("tr"));
+		return localeResolver;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+	    final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+	    messageSource.setBasenames("classpath:/messages");
+	    messageSource.setUseCodeAsDefaultMessage(true);
+	    messageSource.setDefaultEncoding("UTF-8");
+	    messageSource.setCacheSeconds(5);
+	    return messageSource;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}	
 }
